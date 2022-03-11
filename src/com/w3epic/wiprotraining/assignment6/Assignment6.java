f64443fd8a87ce60999074fbd41d9df75cb5f643
Anisa Roy <83353324+royanisa@users.noreply.github.com>
2022-03-12 02:00:01 +0530

    Update Assignment6.java

--- src/com/w3epic/wiprotraining/assignment6/Assignment6.java
+++ src/com/w3epic/wiprotraining/assignment6/Assignment6.java
@@ -12,59 +12,65 @@ import java.util.Scanner;
 
 public class Assignment6 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		for (int i = 0; i < 2; i++) {
-			String name = "";
-			int subA = 0;
-			int subB = 0;
-			int subC = 0;
-			
-			try {				
-				name = sc.nextLine();
-				
-				if (sc.hasNextInt())
-					subA = sc.nextInt();
-				else
-					throw new NumberFormatException();
-				
-				if (sc.hasNextInt())
-					subB = sc.nextInt();
-				else
-					throw new NumberFormatException();
-				
-				if (sc.hasNextInt())
-					subC = sc.nextInt();
-				else
-					throw new NumberFormatException();
-				
-				if (subA < 0) throw new NegativeValuesException();
-				if (subA > 100) throw new ValuesOutOfRangeException();
-				
-				if (subB < 0) throw new NegativeValuesException();
-				if (subB > 100) throw new ValuesOutOfRangeException();
-				
-				if (subC < 0) throw new NegativeValuesException();
-				if (subC > 100) throw new ValuesOutOfRangeException();
-				
-				
-			} catch (ArithmeticException e) {
-				System.out.println(e.getMessage());
-			} catch (NegativeValuesException e) {
-				System.out.println(e.getMessage());
-			} catch (ValuesOutOfRangeException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			System.out.println("Name: " + name);
-			System.out.println("Marks of subject A: " + subA);
-			System.out.println("Marks of subject B: " + subB);
-			System.out.println("Marks of subject C: " + subC);
-		}
-		
-		sc.close();
+	 public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        for (int i = 0; i < 2; i++) {
+            String name = "";
+            int subA = 0;
+            int subB = 0;
+            int subC = 0;
+            int Sum;
+            double avg;
+
+            name = sc.next();
+
+            try {
+
+                if (sc.hasNextInt())
+                    subA = sc.nextInt();
+                else
+                    throw new NumberFormatException();
+
+                if (sc.hasNextInt())
+                    subB = sc.nextInt();
+                else
+                    throw new NumberFormatException();
+
+                if (sc.hasNextInt())
+                    subC = sc.nextInt();
+                else
+                    throw new NumberFormatException();
+
+                if (subA < 0) throw new NegativeValuesException();
+                if (subA > 100) throw new ValuesOutOfRangeException();
+
+                if (subB < 0) throw new NegativeValuesException();
+                if (subB > 100) throw new ValuesOutOfRangeException();
+
+                if (subC < 0) throw new NegativeValuesException();
+                if (subC > 100) throw new ValuesOutOfRangeException();
+
+
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            } catch (NegativeValuesException e) {
+                System.out.println(e.getMessage());
+            } catch (ValuesOutOfRangeException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("Name: " + name);
+            System.out.println("Marks of subject A: " + subA);
+            System.out.println("Marks of subject B: " + subB);
+            System.out.println("Marks of subject C: " + subC);
+            Sum = subA+subB+subC;
+            avg = Sum/3;
+            System.out.println("Average marks of "+name+" is: "+avg);
+        }
+
+        sc.close();
+
+    }
 
 }
